c9c2fcac65fbf7164f2b4168baf9778abec6fcf6
VARUN KHARKWAL <116367329+VKBRAWLER@users.noreply.github.com>
2023-02-21 22:32:29 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,34 @@
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+
+    button.addEventListener('click', (e)=>{
+        if (e.target.innerHTML == "="){
+            let evaluate = document.querySelector('input').value;
+            try {
+                evaluate = eval(evaluate);
+            }
+            catch(e){
+                document.querySelector('input').value = "ERROR";
+            }
+            evaluate = eval(evaluate);
+            document.querySelector('input').value = evaluate;
+        }
+        else if (e.target.innerHTML == "AC"){
+            document.querySelector('input').value = "";
+        }
+        else{
+            let string = "";
+            document.querySelector('input').value += string;
+        }
+    })
+
+    button.addEventListener('mouseover', (f)=>{
+        f.target.style.setProperty('font-size', '75px');
+    })
+    
+    button.addEventListener('mouseout', (g)=>{
+        g.target.style.setProperty('font-size', '50px');
+    })   
+})
+
+// added a comment to update the commit in the github reposoritory
